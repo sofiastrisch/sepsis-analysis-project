4347d42285975e7c45a15f908b467cb9d99f2c19
sofiastrisch <sofia.strisch@gmail.com>
2025-11-11 12:49:50 -0500

    Update sepsis_analysis_bar_graph.py

--- scripts/sepsis_analysis_bar_graph.py
+++ scripts/sepsis_analysis_bar_graph.py
@@ -1,11 +1,10 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-# Load aggregated dataset
+
 data = pd.read_csv('sepsis_project_clean_aggregated.csv')
 
-# Apply your sepsis pathway function
-
+# apply sepsis pathway function
 
 def sepsis_flag(row):
     score = 0
@@ -34,22 +33,22 @@ def sepsis_flag(row):
 
 data['SepsisFlag'] = data.apply(sepsis_flag, axis=1)
 
-# Counts for actual septic patients
+# actual septic patients
 actual_septic_caught = sum(
     (data['SepsisLabel'] == 1) & (data['SepsisFlag'] == 1))
 actual_septic_missed = sum(
     (data['SepsisLabel'] == 1) & (data['SepsisFlag'] == 0))
 
-# Counts for patients flagged by pathway
+# patients flagged by pathway
 flagged_correct = actual_septic_caught
 flagged_false = sum((data['SepsisFlag'] == 1) & (data['SepsisLabel'] == 0))
 
-# Prepare data for plotting
+# prepare data
 categories = ['Actual Septic Patients', 'Patients Flagged by Pathway']
 caught_missed = [actual_septic_caught, flagged_correct]
 missed_false = [actual_septic_missed, flagged_false]
 
-# Plot
+# plotting
 plt.figure(figsize=(8, 6))
 plt.bar(categories, caught_missed,
         label='Correctly flagged / caught', color='darkred')
